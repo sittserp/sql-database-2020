51956998aece831b011a692da94e90ee1760fdeb
Perry Sittser <sittserp@gmail.com>
2020-11-03 19:31:44 -0800

    tests pass!!

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,56 +9,154 @@ const client = require('../lib/client');
 describe('app routes', () => {
   describe('routes', () => {
     let token;
-  
+
     beforeAll(async done => {
       execSync('npm run setup-db');
-  
+
       client.connect();
-  
+
       const signInData = await fakeRequest(app)
         .post('/auth/signup')
         .send({
           email: 'jon@user.com',
           password: '1234'
         });
-      
+
       token = signInData.body.token;
-  
+
       return done();
     });
-  
+
     afterAll(done => {
       return client.end(done);
     });
 
-  test('returns animals', async() => {
+    test('returns trees', async () => {
 
-    const expectation = [
-      {
-        'id': 1,
-        'name': 'bessie',
-        'coolfactor': 3,
-        'owner_id': 1
-      },
-      {
-        'id': 2,
-        'name': 'jumpy',
-        'coolfactor': 4,
-        'owner_id': 1
-      },
-      {
-        'id': 3,
-        'name': 'spot',
-        'coolfactor': 10,
-        'owner_id': 1
-      }
-    ];
+      const expectation = [
+        {
+          id: 1,
+          name: 'fir',
+          hardness_factor: 5,
+          hardwood: false,
+          type: 'coniferous',
+          owner_id: 1,
+        },
+        {
+          id: 2,
+          name: 'maple',
+          hardness_factor: 8,
+          hardwood: true,
+          type: 'deciduous',
+          owner_id: 1,
+        },
+        {
+          id: 3,
+          name: 'oak',
+          hardness_factor: 10,
+          hardwood: true,
+          type: 'deciduous',
+          owner_id: 1,
+        },
+        {
+          id: 4,
+          name: 'cedar',
+          hardness_factor: 6,
+          hardwood: false,
+          type: 'coniferous',
+          owner_id: 1,
+        },
+        {
+          id: 5,
+          name: 'spruce',
+          hardness_factor: 4,
+          hardwood: false,
+          type: 'coniferous',
+          owner_id: 1,
+        },
+        {
+          id: 6,
+          name: 'pine',
+          hardness_factor: 5,
+          hardwood: false,
+          type: 'coniferous',
+          owner_id: 1,
+        },
+        {
+          id: 7,
+          name: 'mahogany',
+          hardness_factor: 7,
+          hardwood: true,
+          type: 'deciduous',
+          owner_id: 1,
+        },
+        {
+          id: 8,
+          name: 'ebony',
+          hardness_factor: 9,
+          hardwood: true,
+          type: 'deciduous',
+          owner_id: 1,
+        }
+      ];
 
-    const data = await fakeRequest(app)
-      .get('/animals')
-      .expect('Content-Type', /json/)
-      .expect(200);
 
-    expect(data.body).toEqual(expectation);
+      const data = await fakeRequest(app)
+        .get('/trees')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+
+    test('returns a single tree', async () => {
+      const expectation = {
+        id: 1,
+        name: 'fir',
+        hardness_factor: 5,
+        hardwood: false,
+        type: 'coniferous',
+        owner_id: 1
+      };
+
+      const data = await fakeRequest(app)
+        .get('/trees/1')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+
+    test('adds a tree to the DB and returns it', async () => {
+      const expectation = {
+        id: 9,
+        name: 'cherry',
+        hardness_factor: 7,
+        hardwood: true,
+        type: 'deciduous',
+        owner_id: 1
+      };
+
+      const data = await fakeRequest(app)
+        .post('/trees')
+        .send({
+          name: 'cherry',
+          hardness_factor: 7,
+          hardwood: true,
+          type: 'deciduous',
+          owner_id: 1
+        })
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      const allTrees = await fakeRequest(app)
+        .get('/trees')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+
+      expect(data.body).toEqual(expectation);
+      expect(allTrees.body.length).toEqual(9);
+    });
   });
 });
